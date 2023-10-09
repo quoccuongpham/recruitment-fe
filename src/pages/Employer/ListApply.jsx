1b6caed78a0690a55773e2e3e4cfb2152fe51b27
cuongpham <quoccuonga2st@gmail.com>
2023-10-09 21:24:46 +0700

    accept apply

--- src/pages/Employer/ListApply.jsx
+++ src/pages/Employer/ListApply.jsx
@@ -1,21 +1,24 @@
 import { Container } from "@mui/material";
 import AccountApply from "../../components/Employer/list-apply-accout";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+
 const ListApply = () => {
-    const list_info = useLoaderData();
-    return (
-        <Container
-            sx={{
-                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-                padding: "10px 0",
-                borderRadius: "10px",
-            }}
-        >
-            {list_info.map((value, index) => {
-                return <AccountApply info={value} key={index} />;
-            })}
-        </Container>
-    );
+	const list_info = useLoaderData();
+	const id_job = useParams().id;
+	console.log(list_info);
+	return (
+		<Container
+			sx={{
+				boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+				padding: "10px 0",
+				borderRadius: "10px",
+			}}
+		>
+			{list_info.map((value, index) => {
+				return <AccountApply info={{ id_job, ...value }} key={index} />;
+			})}
+		</Container>
+	);
 };
 
 export default ListApply;
